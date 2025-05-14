2aaf476b2cf6f2e2e11a29de81d7a179d980bae5
Marcodellavecchia95 <mdellavecchia95@gmail.com>
2025-05-14 15:48:38 +0200

    add bonus

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,15 +1,38 @@
 const { posts } = require("../data/posts");
 
 const index = (req, res) => {
+  let filterTag = req.query.tags;
+  let filteredPosts = [...posts];
+  let message = "Post trovati";
+  if (filterTag) {
+    filteredPosts = filteredPosts.filter((post) =>
+      post.tags.includes(filterTag)
+    );
+    if (filteredPosts.length === 0) {
+      message = `Non sono stati trovati post con la parola ${filterTag}`;
+    } else {
+      message = `Post che includono la parola ${filterTag}`;
+    }
+  }
+
   res.json({
-    description: "Ecco la lista degli elementi",
-    data: posts,
+    message,
+    data: filteredPosts,
   });
 };
 
 const show = (req, res) => {
   const id = parseInt(req.params.id);
   const post = posts.find((currentPost) => currentPost.id === id);
+
+  if (!post) {
+    res.status(404).json({
+      error: "404, not found",
+      message: "Post non trovato",
+    });
+    return;
+  }
+
   res.json({
     description: `${post.title}`,
     data: post,
@@ -42,7 +65,7 @@ const destroy = (req, res) => {
 
   if (!post) {
     res.status(404).json({
-      error: "404, non trovato",
+      error: "404, not found",
       message: "Post non trovato",
     });
     return;
