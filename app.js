09b314596bdfdd15273c6bb6ec57eeb6f6a483f0
Marcodellavecchia95 <mdellavecchia95@gmail.com>
2025-05-16 17:22:11 +0200

    add notFound.js

--- app.js
+++ app.js
@@ -4,12 +4,14 @@ const port = 3000;
 const appUrl = `http://localhost:${port}`;
 const postsRouter = require("./routers/router");
 const errorHandler = require("./middlewares/errorHandler");
+const notFound = require("./middlewares/notFound");
 
 app.use(express.static("public"));
 app.use(express.json());
 
 app.use("/posts", postsRouter);
 
+app.use(notFound);
 app.use(errorHandler);
 
 app.listen(port, () => {
