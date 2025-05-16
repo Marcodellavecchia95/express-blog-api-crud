2dfba02be4dec07d64036a2205d359eb4433a499
Marcodellavecchia95 <mdellavecchia95@gmail.com>
2025-05-16 16:01:01 +0200

    add middlewares folder and errorHandler.js

--- app.js
+++ app.js
@@ -3,11 +3,14 @@ const app = express();
 const port = 3000;
 const appUrl = `http://localhost:${port}`;
 const postsRouter = require("./routers/router");
+const errorHandler = require("./middlewares/errorHandler");
 
 app.use(express.static("public"));
 app.use(express.json());
 
 app.use("/posts", postsRouter);
+
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`Server listening on ${appUrl}`);
